8a3d4e1b1efe3389613bcff6aec0cf4b8fbeff17
kuohakuu <110683088+mCszao@users.noreply.github.com>
2022-12-09 15:26:01 -0400

    alteração na estrutura js

--- ping-pong/index.js
+++ ping-pong/index.js
@@ -1,17 +1,14 @@
 const first = document.querySelector("#ftScreen");
 let animation;
-let px = 0;
+let py = 0;
 
-window.addEventListener("load", draw);
-
-function draw() {
+let moviment = function () {
   let ftContext = first.getContext("2d");
 
-  ftContext.clearRect(0, 0, 500, 500);
-
+  ftContext.clearRect(0, 0, 600, 600);
   ftContext.fillStyle = "#fff";
-  ftContext.fillRect(px, 0, 50, 50);
-
-  px++;
-  animation = requestAnimationFrame(draw);
-}
+  ftContext.fillRect(0, py, 50, 50);
+  window = requestAnimationFrame(moviment);
+  py++;
+};
+window.onload = moviment;
